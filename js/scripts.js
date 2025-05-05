b18008d4b2493883d35f1b7b9642f30e387ff61b
n-orellana <nicolas.orellana.index@gmail.com>
2025-05-05 20:41:32 -0300

    professionalPortfolio

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
+
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
+
+const langButtons = document.querySelectorAll("[data-language]");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+langButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        fetch(`../languages/${button.dataset.language}.json`)
+            .then(res => res.json())
+            .then(data => {
+                textsToChange.forEach((el) => {
+                    const section = el.dataset.section;
+                    const value = el.dataset.value;
+
+                    el.innerHTML = data[section][value];
+                })
+            })
+    })
+})
+/*
+let imgContainer = document.querySelector(".lang_button");
+let img = imgContainer.querySelector("img");
+
+let newImg = document.createElement("img");
+newImg.classList.add("img-blur");
+newImg.src = img.src;
+
+imgContainer.prepend(newImg);*/
+
+toggleTheme.addEventListener("click", ()=> {
+    document.body.classList.toggle("dark");
+    if(toggleIcon.src.includes("moon.svg")){
+        toggleIcon.src = "assets/icons/sun.svg";
+        toggleText.textContent = "Light Mode";
+    }
+    else{
+        toggleIcon.src = "assets/icons/moon.svg";
+        toggleText.textContent = "Dark Mode";
+    }
+});
+
+toggleColors.addEventListener("click", (e) => {
+    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+});
